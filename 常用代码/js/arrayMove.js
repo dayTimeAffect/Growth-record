d17cccc54522e3abedce8eff9212f965b042773f
DESKTOP-B9RPFLK\karakal <jun.deng@karakal.com.cn>
2020-08-28 15:30:09 +0800

    2020/8/28  数组移动函数

---
+++ 常用代码/js/arrayMove.js
@@ -0,0 +1,11 @@
+const arrayMove = (arr, start, end) => {
+    if (start >= arr.length || end >= arr.length) return ;
+    while (start < 0 || end < 0){
+        start = start < 0 ? arr.length + start : start
+        end = end < 0 ? arr.length + end : end
+    }
+    const [moveItem] = arr.splice(start, 1)
+    arr.splice(end, 0, moveItem)
+    return arr
+}
+console.log(arrayMove([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], -10, -1));

e44ad13b2e467d64f0899ef13845d5086c8147d5
DESKTOP-B9RPFLK\karakal <jun.deng@karakal.com.cn>
2020-09-14 18:26:52 +0800

    2020/9/14算法题

---
+++ leetcode/腾讯精选练习题/螺旋矩阵 II.js
@@ -0,0 +1,60 @@
+/*
+* 给定一个正整数 n，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵。
+* */
+/**
+ * @param {number} n
+ * @return {number[][]}
+ */
+var generateMatrix = function(n) {
+    let result = new Array(n).fill(-1)
+    result = result.map((v) => new Array(n).fill(-1))
+    result[0][0] = 1
+    let direction = 'x' //方向
+    let operation = +1 //操作
+    let index = 2
+    let y = 0, x = 0
+    while (index <= n*n){
+        if (direction === 'y') {
+            if (result[y + operation] && result[y + operation][x] === -1) {
+                result[y + operation][x] = index
+                y = y + operation
+                index ++
+            }else{
+                direction = 'x'
+                operation = x === y ? -1 : +1
+            }
+
+        }
+        if (direction === 'x') {
+            if (result[y] && result[y][x + operation] === -1) {
+                result[y][x + operation] = index
+                x = x + operation
+                index ++
+            }else{
+                direction = 'y'
+                operation = x < y ? -1 : +1
+            }
+        }
+
+    }
+    return result
+};
+// console.log(generateMatrix(3));
+
+var generateMatrix_best = function(n) {
+    let result = new Array(n).fill(-1)
+    result = result.map((v) => new Array(n).fill(-1))
+    let index = 1, target = n * n, left = 0, top = 0, right = n - 1, bottom = n - 1;
+    while (index <= target){
+        for (let i = left; i <= right; i ++) result[top][i] = index ++
+        top ++
+        for (let i = top; i <= bottom; i ++) result[i][right] = index ++
+        right --
+        for (let i = right; i >= left; i --) result[bottom][i] = index ++
+        bottom --
+        for (let i = bottom; i >= top; i --) result[i][left] = index ++
+        left ++
+    }
+    return result
+};
+console.log(generateMatrix_best(3));
--- 常用代码/js/arrayMove.js
+++ 常用代码/js/arrayMove.js
@@ -8,4 +8,4 @@ const arrayMove = (arr, start, end) => {
     arr.splice(end, 0, moveItem)
     return arr
 }
-console.log(arrayMove([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], -10, -1));
+console.log(arrayMove([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 11, -1));
